d8ce43f963ce23ceaab526ba61e19d3c0d8c2f49
retroider <gen.hort54@gmail.com>
2026-02-19 17:24:23 +0300

    change ai client

--- src/main/java/com/galaxy13/tutor/service/AiService.java
+++ src/main/java/com/galaxy13/tutor/service/AiService.java
@@ -1,9 +1,15 @@
 package com.galaxy13.tutor.service;
 
+import com.galaxy13.tutor.exception.ResourceNotFoundException;
+import com.galaxy13.tutor.model.Chat;
+import com.galaxy13.tutor.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -11,9 +17,22 @@ public class AiService {
 
     private final ChatClient chatClient;
 
-    public String chat(String prompt, String conversationId) {
-        return chatClient.prompt(prompt)
-                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
-                .call().content();
+    private final ChatRepository chatRepository;
+
+    public String chat(UUID chatId, String userMessage) {
+        Chat chat = chatRepository.findChatById(chatId)
+                .orElseThrow(() -> new ResourceNotFoundException("Chat nor found with id:" + chatId));
+        String prompt = promptBeautify(chat.getPrompt().getContent());
+
+        return chatClient.prompt()
+                .system(prompt)
+                .user(userMessage)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, chatId.toString()))
+                .call()
+                .content();
+    }
+
+    private String promptBeautify(Map<String, Object> prompt) {
+        return "";
     }
 }
